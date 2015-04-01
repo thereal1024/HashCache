9865463b0454c4d0a012dc4c3b646868929f92e6
Steve Halm <thereal1024@gmail.com>
2015-03-31 19:10:56 -0500

    add working db schema

---
+++ dbinit.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE HashCache;
+USE HashCache;
+
+CREATE TABLE Transaction(
+	transactionID BINARY(32) PRIMARY KEY,
+	hasFailed TINYINT(1) NOT NULL,
+	confirmations INTEGER,
+	includedBlock BINARY(32),
+	fee BIGINT UNSIGNED, -- in satoshis
+	address CHAR(36),
+	txtime TIMESTAMP
+);
+
+CREATE TABLE MerkleRoot(
+	nodeID BINARY(32) PRIMARY KEY
+);
+
+CREATE TABLE Proof(
+	transactionID BINARY(32) PRIMARY KEY,
+	windowID INTEGER NOT NULL
+);
+
+CREATE TABLE Paternal(
+	nodeID INTEGER PRIMARY KEY,
+	parentID INTEGER
+);
+
+CREATE TABLE Siblings(
+	leftID INTEGER PRIMARY KEY,
+	rightID INTEGER
+);
+
+CREATE TABLE NodeHash(
+	nodeID INTEGER PRIMARY KEY AUTO_INCREMENT,
+	windowID INTEGER NOT NULL,
+	treeLevel INTEGER NOT NULL,
+	hash BINARY(32) NOT NULL
+);
+
+CREATE TABLE SubmittedHashes(
+	hash BINARY(32) PRIMARY KEY,
+	uploadTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Window(
+	windowID INTEGER PRIMARY KEY AUTO_INCREMENT,
+	startTime TIMESTAMP NOT NULL,
+	endTime TIMESTAMP NOT NULL
+);
