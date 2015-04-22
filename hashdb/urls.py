0efc8043978534c15fbdbfe3e46359e980c9c8b8
Steve Halm <thereal1024@gmail.com>
2015-04-22 00:44:02 -0500

    static app files

--- hashdb/urls.py
+++ hashdb/urls.py
@@ -3,9 +3,16 @@ from django.conf.urls import patterns, url
 from hashdb import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
     url(r'^api/window/open$', views.open_window, name='api-open-window'),
 	url(r'^api/window/id/([0-9]+)$', views.view_window, name='api-view-window'),
 	url(r'^api/hashes$', views.submit_hash, name='api-submit-hash'),
 	url(r'^api/hashes/([0-9a-fA-F]{64})$', views.hash_info, name='api-hash-info'),
 )
+
+urlpatterns += patterns(
+    'django.contrib.staticfiles.views',
+    url(r'^(?:index.html)?$', 'serve', kwargs={'path': 'index.html'}),
+    url(r'^(?P<path>(?:js|css|img)/.*)$', 'serve'),
+)
+
